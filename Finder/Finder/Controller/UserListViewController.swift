325d8b7751d9a7eb76a9269675eba75fe2cad27d
Onur Ustunel <ustunel.onur@gmail.com>
2021-11-24 22:42:40 +0300

    initial commit for Finder

---
+++ Finder/Finder/Controller/UserListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UserListViewController.swift
+//  Finder
+//
+//  Created by Onur Ustunel on 24.11.2021.
+//
+
+import UIKit
+
+class UserListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+
+
+}
+
